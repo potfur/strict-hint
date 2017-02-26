95a4dea19fed6cfdae2556c3becede1fae4ac960
potfur <wachowski.michal@gmail.com>
2017-02-26 20:08:57 +0100

    adjust repo links + classifiers

--- setup.py
+++ setup.py
@@ -9,12 +9,18 @@ setup(
     description='Strict type runtime checks for arguments and return types.',
     author='Michal Wachowski',
     author_email='wachowski.michal@gmail.com',
-    url='https://github.com/potfur/strict_hint',
-    download_url='https://github.com/potfur/strict_hint/tarball/0.1',
+    url='https://github.com/potfur/strict-hint',
+    download_url='https://github.com/potfur/strict-hint/tarball/0.1',
     keywords=['type hint', 'type validation', 'type declaration', 'argument type', 'return type'],
     tests_require=[
         'pytest',
         'pytest-flake8',
     ],
-    classifiers=[],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
 )
