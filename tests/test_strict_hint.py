1dafd5231f95c7fdbb031e8f394ec15f949f647f
Michal Wachowski <wachowski.michal@gmail.com>
2017-04-19 21:48:09 +0200

    drop duplicated tests

--- tests/test_strict_hint.py
+++ tests/test_strict_hint.py
@@ -2,13 +2,12 @@ from types import FunctionType
 
 from pytest import raises
 
-from strict_hint.strict_hint import StrictHint
-from tests import Bar
-from tests import Foo
+from strict_hint import strict
+from tests import Foo, Bar
 
 
-def test_accept_any_arg_when_no_annotation():
-    @StrictHint()
+def test_func_alias_func_alias_accept_any_arg_when_no_annotation():
+    @strict
     def func(r):
         return r
 
@@ -16,24 +15,24 @@ def test_accept_any_arg_when_no_annotation():
     assert func('lorem ipsum') == 'lorem ipsum'
 
 
-def test_accept_arg_with_value_matching_annotation():
-    @StrictHint()
+def test_func_alias_accept_arg_with_value_matching_annotation():
+    @strict
     def func(r: int):
         return r
 
     assert func(1) == 1
 
 
-def test_accept_arg_with_value_matching_standard_interpreter_type():
-    @StrictHint()
+def test_func_alias_accept_arg_with_value_matching_standard_interpreter_type():
+    @strict
     def func(r: FunctionType):
         return r
 
-    assert func(func) == func
+    assert func(strict) == strict
 
 
-def test_raise_type_error_arg_value_does_not_match_annotation():
-    @StrictHint()
+def test_func_alias_raise_type_error_arg_value_does_not_match_annotation():
+    @strict
     def func(r: int):
         return r
 
@@ -44,24 +43,24 @@ def test_raise_type_error_arg_value_does_not_match_annotation():
                            " <class 'int'>, <class 'str'> given"
 
 
-def test_accept_default_value_even_when_different_type():
-    @StrictHint()
+def test_func_alias_accept_default_value_even_when_different_type():
+    @strict
     def func(r: int = 'foo'):
         return r
 
     assert func() == 'foo'
 
 
-def test_accept_value_equal_to_default_value_when_different_type():
-    @StrictHint()
+def test_func_alias_accept_value_equal_to_default_value_when_different_type():
+    @strict
     def func(r: int = 'foo'):
         return r
 
     assert func('foo') == 'foo'
 
 
-def test_accept_value_when_matches_one_of_annotated_types():
-    @StrictHint()
+def test_func_alias_accept_value_when_matches_one_of_annotated_types():
+    @strict
     def func(a: (int, str)):
         pass
 
@@ -69,8 +68,8 @@ def test_accept_value_when_matches_one_of_annotated_types():
     func('lorem ipsum')
 
 
-def test_raise_type_error_when_value_is_not_in_annotated_tuple():
-    @StrictHint()
+def test_func_alias_raise_type_error_when_value_is_not_in_annotated_tuple():
+    @strict
     def func(r: (int, str)):
         return r
 
@@ -83,7 +82,7 @@ def test_raise_type_error_when_value_is_not_in_annotated_tuple():
 
 
 def test_func_alias_raise_type_error_when_value_is_not_a_list_of():
-    @StrictHint()
+    @strict
     def func(r: [str]):
         return r
 
@@ -95,8 +94,8 @@ def test_func_alias_raise_type_error_when_value_is_not_a_list_of():
                     " [<class 'str'>], <class 'int'> given"
 
 
-def test_accepts_any_return_value_if_no_annotation():
-    @StrictHint()
+def test_func_alias_accepts_any_return_value_if_no_annotation():
+    @strict
     def func(r):
         return r
 
@@ -104,24 +103,24 @@ def test_accepts_any_return_value_if_no_annotation():
     assert func('lorem ipsum') == 'lorem ipsum'
 
 
-def test_accept_return_value_matching_annotation():
-    @StrictHint()
+def test_func_alias_accept_return_value_matching_annotation():
+    @strict
     def func(r) -> str:
         return r
 
     assert func('lorem ipsum') == 'lorem ipsum'
 
 
-def test_accept_return_value_matching_standard_interpreter_type():
-    @StrictHint()
+def test_func_alias_accept_return_value_matching_standard_interpreter_type():
+    @strict
     def func(r) -> FunctionType:
         return r
 
-    assert func(func) == func
+    assert func(strict) == strict
 
 
-def test_accept_return_value_matching_annotated_tuple():
-    @StrictHint()
+def test_func_alias_accept_return_value_matching_annotated_tuple():
+    @strict
     def func(r) -> (int, str):
         return r
 
@@ -129,8 +128,8 @@ def test_accept_return_value_matching_annotated_tuple():
     assert func('lorem ipsum') == 'lorem ipsum'
 
 
-def test_raise_error_when_value_does_not_match_annotation():
-    @StrictHint()
+def test_func_alias_raise_error_when_value_does_not_match_annotation():
+    @strict
     def func(r) -> str:
         return r
 
@@ -141,8 +140,8 @@ def test_raise_error_when_value_does_not_match_annotation():
                            " <class 'str'>, <class 'int'> returned"
 
 
-def test_raise_error_when_value_does_not_match_annoted_tuple():
-    @StrictHint()
+def test_func_alias_raise_error_when_value_does_not_match_annoted_tuple():
+    @strict
     def func(r) -> (int, str):
         return r
 
@@ -155,7 +154,7 @@ def test_raise_error_when_value_does_not_match_annoted_tuple():
 
 
 def test_raise_error_when_value_is_not_list_of():
-    @StrictHint()
+    @strict
     def func(r) -> [str]:
         return r
 
@@ -167,24 +166,24 @@ def test_raise_error_when_value_is_not_list_of():
                     " [<class 'str'>], <class 'int'> returned"
 
 
-def test_accept_user_defined_class():
-    @StrictHint()
+def test_func_alias_accept_user_defined_class():
+    @strict
     def func(r: Foo) -> Foo:
         return r
 
     assert func(Foo())
 
 
-def test_accept_user_defined_class_inheritance():
-    @StrictHint()
+def test_func_alias_accept_user_defined_class_inheritance():
+    @strict
     def func(r: object) -> object:
         return r
 
     assert func(Foo())
 
 
-def test_raises_error_when_different_user_defined_class_passed():
-    @StrictHint()
+def test_func_alias_raises_error_when_different_user_defined_class_passed():
+    @strict
     def func(r: Foo) -> Foo:
         return r
 
@@ -196,7 +195,7 @@ def test_raises_error_when_different_user_defined_class_passed():
 
 
 def test_raises_error_when_required_argument_omitted():
-    @StrictHint()
+    @strict
     def func(r: Foo) -> Foo:
         return r
 
@@ -207,7 +206,7 @@ def test_raises_error_when_required_argument_omitted():
 
 
 def test_raises_error_when_optional_argument_omitted():
-    @StrictHint()
+    @strict
     def func(a, r: Foo = 2) -> tuple:
         return a, r
 
@@ -215,8 +214,8 @@ def test_raises_error_when_optional_argument_omitted():
 
 
 def test_raised_error_includes_class_name():
-    class Yada(object):
-        @StrictHint()
+    class Yada:
+        @strict
         def func(self, r: Foo) -> Foo:
             return r
 
